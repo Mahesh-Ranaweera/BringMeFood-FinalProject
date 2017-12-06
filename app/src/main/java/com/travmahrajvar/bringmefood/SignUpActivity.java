e462344da715860701de85c6a6c1b0c96090c5ec
Mahesh Ranaweera <mi6softlab@gmail.com>
2017-12-06 01:30:01 -0500

    Connection made to firebase storage

--- app/src/main/java/com/travmahrajvar/bringmefood/SignUpActivity.java
+++ app/src/main/java/com/travmahrajvar/bringmefood/SignUpActivity.java
@@ -5,14 +5,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+
 public class SignUpActivity extends AppCompatActivity {
 
     EditText txtEmail, txtUsername, txtFName, txtLName, txtPassw1, txtPassw2;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("msg");
+
+        myRef.setValue("Testing Firebase storage");
 
         txtEmail = (EditText) findViewById(R.id.txtEmail);
         txtUsername = (EditText) findViewById(R.id.txtUsername);
