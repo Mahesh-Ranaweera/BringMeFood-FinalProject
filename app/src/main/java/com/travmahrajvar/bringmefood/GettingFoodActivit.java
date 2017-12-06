a47231f312c2bbd11806eca3cea04779c7f0d5b3
Mahesh Ranaweera <mi6softlab@gmail.com>
2017-12-06 00:15:30 -0500

    FOOOD

--- app/src/main/java/com/travmahrajvar/bringmefood/GettingFoodActivit.java
+++ app/src/main/java/com/travmahrajvar/bringmefood/GettingFoodActivit.java
@@ -2,12 +2,30 @@ package com.travmahrajvar.bringmefood;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class GettingFoodActivit extends AppCompatActivity {
+
+    Spinner restList;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getting_food);
+
+        restList = (Spinner) findViewById(R.id.spinner);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Starbucks");
+        list.add("Tim Hortans");
+        list.add("McDonalds");
+
+        adapter = new ArrayAdapter<String>(this, R.layout.spinner_list_style, list);
+        restList.setAdapter(adapter);
     }
 }
