963731f2acda6d22071247b28eabdfbc5d8b19cb
Travis Madill <TravisMadill@gmail.com>
2017-12-11 14:53:33 -0500

    Preliminary getting food activity with tabs

--- app/src/main/java/com/travmahrajvar/bringmefood/GettingFoodActivity.java
+++ app/src/main/java/com/travmahrajvar/bringmefood/GettingFoodActivity.java
@@ -1,24 +1,15 @@
 package com.travmahrajvar.bringmefood;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
 import android.widget.PopupMenu;
-import android.widget.TextView;
 
 import com.travmahrajvar.bringmefood.utils.FirebaseHandler;
 
@@ -81,43 +72,10 @@ public class GettingFoodActivity extends AppCompatActivity {
 	
 	
 	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		private static final String ARG_SECTION_NUMBER = "section_number";
-		
-		public PlaceholderFragment() {
-		}
-		
-		/**
-		 * Returns a new instance of this fragment for the given section
-		 * number.
-		 */
-		public static PlaceholderFragment newInstance(int sectionNumber) {
-			PlaceholderFragment fragment = new PlaceholderFragment();
-			Bundle args = new Bundle();
-			args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-			fragment.setArguments(args);
-			return fragment;
-		}
-		
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-		                         Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_getting_food, container, false);
-			TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-			textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-			return rootView;
-		}
-	}
-	
-	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
 	 * one of the sections/tabs/pages.
+	 *
+	 * (Created from the default New Tabbed Activity section in Android Studio.)
 	 */
 	public class SectionsPagerAdapter extends FragmentPagerAdapter {
 		
@@ -127,9 +85,16 @@ public class GettingFoodActivity extends AppCompatActivity {
 		
 		@Override
 		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a PlaceholderFragment (defined as a static inner class below).
-			return PlaceholderFragment.newInstance(position + 1);
+			//Set each tab as appropriate
+			switch(position){
+				case 1: // Approved users
+					return GettingFoodFragment_ApprovedUsers.newInstance("", "");
+				case 2: // Pending users
+					return GettingFoodFragment_PendingUsers.newInstance("", "");
+				case 0: // Session info
+				default:
+					return GettingFoodFragment_SessionInfo.newInstance("", "");
+			}
 		}
 		
 		@Override
